ba0406b7c49f8ae249737b79c7b8e7598a873233
campa <francesco.camp@gmail.com>
2019-06-11 15:11:55 +0200

    Added statsActivity for biker

--- deliveryman/src/main/java/com/example/biker/MainActivity.java
+++ deliveryman/src/main/java/com/example/biker/MainActivity.java
@@ -359,6 +359,15 @@ public class MainActivity extends AppCompatActivity {
             Intent i = new Intent(getApplicationContext(), OrderActivity.class);
             startActivity(i);
         }
+        else if(title.equals("STATS")){
+            if(FirebaseAuth.getInstance().getCurrentUser()==null){
+                Toast.makeText(this, "Error: user not signed in.",
+                        Toast.LENGTH_SHORT).show();
+                return;
+            }
+            Intent i = new Intent(getApplicationContext(), StatsActivity.class);
+            startActivity(i);
+        }
         if(title.equals("SIGN UP")) {
             Intent i = new Intent(getApplicationContext(), SignupActivity.class);
             startActivity(i);
---
+++ deliveryman/src/main/java/com/example/biker/StatsActivity.java
@@ -0,0 +1,87 @@
+package com.example.biker;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
+
+import java.util.Objects;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class StatsActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser currentUser;
+    private DatabaseReference database;
+    private StorageReference storage;
+    private TextView km_traveled_tv;
+    private TextView orders_qty_tv;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stats);
+
+        mAuth= FirebaseAuth.getInstance();
+        currentUser=mAuth.getCurrentUser();
+        database= FirebaseDatabase.getInstance().getReference();
+        storage= FirebaseStorage.getInstance().getReference();
+
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        km_traveled_tv = findViewById(R.id.km_traveled_tv);
+        orders_qty_tv = findViewById(R.id.orders_qty_tv);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        String uid=currentUser.getUid();
+
+        database.child("biker").child(uid).child("stats").child("dist").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(!(dataSnapshot.getValue()==null))
+                    km_traveled_tv.setText(dataSnapshot.getValue().toString()+" kilometers traveled");
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+        database.child("biker").child(uid).child("stats").child("num").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(!(dataSnapshot.getValue()==null))
+                    orders_qty_tv.setText("Orders delivered: "+dataSnapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+}
